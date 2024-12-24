dcb296e607f9c3c97512233b66e3869ccd2d6a05
Scorpion197 <doudou.gaw@gmail.com>
2024-12-24 20:21:29 +0100

    chore: change APIs address

--- baladiyatuna/src/config/index.js
+++ baladiyatuna/src/config/index.js
@@ -1,16 +1,15 @@
-const API_URL = 'http://137.74.214.49/back/baladiya/';
+const API_URL = "http://51.255.8.253/baladiya/";
 
 export const setToken = (token) => {
-    sessionStorage.setItem('token',token);
-}
+  sessionStorage.setItem("token", token);
+};
 
 export const getToken = (token) => {
-    return sessionStorage.getItem('token')
-}
+  return sessionStorage.getItem("token");
+};
 
 export const removeToken = () => {
-    sessionStorage.removeItem('token');
-}
-
+  sessionStorage.removeItem("token");
+};
 
 export { API_URL };
